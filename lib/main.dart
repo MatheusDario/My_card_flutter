685684968f477e30f8b02a0ec46db44af512363c
Matheus Dario <dario.matheus97@gmail.com>
2023-08-13 23:23:20 -0300

    projeto my card em andamento

--- lib/main.dart
+++ lib/main.dart
@@ -11,49 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  const MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.orange,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.white,
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                  Container(
-                    color: Colors.white,
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.black,
-                height: 150.0,
-                width: 200.0,
-                child: Center(child: Text( style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold),'Desafio aula 15')),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.white,
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                  Container(
-                    color: Colors.white,
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                ],
-              )
+              CircleAvatar(radius: 70.0, backgroundImage: AssetImage('images/perfil.png')),
+              Text('Matheus Dario', style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold),),
             ],
           )
         ),
